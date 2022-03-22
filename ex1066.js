8549f58807562d9993679648b20601bdc4fe6c1e
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2022-03-22 14:35:42 -0300

    Pares, Ímpares, Positivos e Negativos

--- ex1066.js
+++ ex1066.js
@@ -1,5 +1,5 @@
 // entrada
-let input = require('fs').readFileSync('dev/stdin', 'utf-8'); //adicionar uma barra antes do 'dev' para funcionar no beecrowd
+let input = require('fs').readFileSync('dev/stdin', 'utf-8').split('\n'); //adicionar uma barra antes do 'dev' para funcionar no beecrowd
 
 // processamento
 let cont_pares = 0;
@@ -7,13 +7,13 @@ let cont_impares = 0;
 let cont_positivos = 0;
 let cont_negativos = 0;
 
-input.split('\n').map(v => {
-    let x = parseInt(v);
-    if(x % 2 === 0) cont_pares++;
+for(let i = 0; i < 5; i++){
+    let elem = parseInt(input.shift());
+    if(elem % 2 === 0) cont_pares++;
     else cont_impares++;
-    if(x > 0) cont_positivos++;
-    else if(x < 0) cont_negativos++;
-});
+    if(elem > 0) cont_positivos++;
+    else if(elem < 0) cont_negativos++; 
+}
 
 // saida
 console.log(cont_pares + ' valor(es) par(es)');
