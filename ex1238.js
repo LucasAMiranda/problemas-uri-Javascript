d75260c77b18d4dc4f839dba761812b7da645977
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2022-03-25 16:01:30 -0300

    Combinador

--- ex1238.js
+++ ex1238.js
@@ -1,5 +1,5 @@
 // input
-let input = require('fs').readFileSync('/dev/stdin', 'utf-8').split('\n'); // adicione uma barra "/" antes do "dev" para ser aceito no beecrowd
+let input = require('fs').readFileSync('dev/stdin', 'utf-8').split('\n'); // adicione uma barra "/" antes do "dev" para ser aceito no beecrowd
 
 // processing
 let cases_test = parseInt(input.shift());
@@ -8,7 +8,7 @@ while(cases_test > 0){
     cases_test--;
 
     let [word_1, word_2] = input.shift().split(' ');
-
+    
     const combiner = (word_x, word_y) => {
         let combined_word = String();
         let index_word_x = 0;
@@ -18,11 +18,11 @@ while(cases_test > 0){
 
         while(size_word_x > 0 || size_word_y > 0){
             if(size_word_x > 0){
-                combined_word.concat(word_x[index_word_x++]);
+                combined_word += word_x[index_word_x++];
                 size_word_x--;
             }
             if(size_word_y > 0){
-                combined_word.concat(word_y[index_word_y++]);
+                combined_word += word_y[index_word_y++];
                 size_word_y--;
             }
         }
